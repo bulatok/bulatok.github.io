6c2ad2e50182930db6aa6d3c7f8c21a0006a78a1
bulatok <bulat2020205@gmail.com>
2022-07-03 16:49:00 +0300

    trying to fix bug

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,25 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
+    adapter: adapter({
+		// default options are shown
+		pages: "build",
+		assets: "build",
+		fallback: null,
+	  }),
+
 	kit: {
 		adapter: adapter(),
 		paths:{
-			base: 'https://github.com/bulatok/bulatok.github.io',
+			base: dev ? '' : 'https://github.com/bulatok/bulatok.github.io',
 		}
 	}
 };
