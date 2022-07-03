c29ec4334315f8efb042d6113ffeaf8f7eec2695
bulatok <bulat2020205@gmail.com>
2022-07-03 16:45:22 +0300

    changed configs

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,10 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths:{
+			base: 'https://github.com/bulatok/bulatok.github.io',
+		}
 	}
 };
 
