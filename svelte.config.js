adc365aae80ffe98062bcaaf580a4d98cf60600a
bulatok <bulat2020205@gmail.com>
2022-07-06 14:03:03 +0300

    .prettierignore configured

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,24 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
-		serviceWorker: {
-			register: false,
-		},
-		paths: {
-			// base: dev ? '' : '/bulatok.github.io',
-		},
-		prerender: {
-			default: true,
-		},
-	},
+  kit: {
+    adapter: adapter(),
+    serviceWorker: {
+      register: false,
+    },
+    paths: {
+      // base: dev ? '' : '/bulatok.github.io',
+    },
+    prerender: {
+      default: true,
+    },
+  },
 };
 
 export default config;
